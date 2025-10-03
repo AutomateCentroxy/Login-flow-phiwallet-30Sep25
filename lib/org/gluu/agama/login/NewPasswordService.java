86ed1f7fd6a74ff59fd6310bbff04594fe518a47
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-03 17:03:42 +0530

    feat: add NewPasswordService.java

---
+++ lib/org/gluu/agama/login/NewPasswordService.java
@@ -0,0 +1,25 @@
+package org.gluu.agama.login;
+
+import org.gluu.agama.login.jans.JansNewPasswordService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class NewPasswordService {
+
+    public abstract boolean validate(String username, String password);
+
+    public abstract String lockAccount(String username);
+
+    public abstract boolean isPhoneUnique(String username, String phone);
+
+    public abstract boolean markPhoneAsVerified(String username, String phone);
+
+    public abstract boolean sendOTPCode(String username, String phone);
+
+    public abstract boolean validateOTPCode(String phone, String code);
+
+    public static NewPasswordService getInstance(HashMap config) {
+        return new JansNewPasswordService(config);
+    }
+}
