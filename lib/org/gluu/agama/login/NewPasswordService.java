dcf65dca36d37e429f871b88c3738e2450b7c74f
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-03 21:01:44 +0530

    refactor: update NewPasswordService.java

--- lib/org/gluu/agama/login/NewPasswordService.java
+++ lib/org/gluu/agama/login/NewPasswordService.java
@@ -11,7 +11,6 @@ public abstract class NewPasswordService {
 
     public abstract String lockAccount(String username);
     
-    public abstract boolean isPhoneUnique(String username, String phone);
 
     public abstract boolean isPhoneUnique(String username, String phone);
 
